22fb65759cd50ec2214fd3fed376a01c4b41f98f
PMunne <Pim.Munne@gmail.com>
2021-06-09 17:12:35 +0200

    added buffs to all monsters

--- src/main/java/nl/pjiwm/kaizominecraft/listeners/SpawnBuffedMob.java
+++ src/main/java/nl/pjiwm/kaizominecraft/listeners/SpawnBuffedMob.java
@@ -2,6 +2,8 @@ package nl.pjiwm.kaizominecraft.listeners;
 
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
@@ -15,30 +17,31 @@ public class SpawnBuffedMob implements Listener {
 
     @EventHandler
     public void spawnEntity(EntitySpawnEvent e) {
-        switch(e.getEntity().getType()) {
+        Entity entity = e.getEntity();
+        if(entity instanceof Monster) {
+            buffMonster(entity);
+        }
+
+        switch(entity.getType()) {
             case CREEPER:
             Creeper creeper = (Creeper) e.getEntity();
             buffCreeper(creeper);
         }
     }
 
-    private Collection<PotionEffect> getEffects() {
-        int duration = 100000;
-        Collection<PotionEffect> effects = new ArrayList<>();
-        effects.add(new PotionEffect(PotionEffectType.SPEED, duration, 2, false, false));
-        effects.add(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration,1, false, false));
-        return effects;
-    }
-
     private void buffCreeper(Creeper creeper) {
         creeper.setPowered(true);
         creeper.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(30);
         creeper.setExplosionRadius(20);
-        creeper.addPotionEffects(getEffects());
     }
 
-    private void cow() {
+    private void buffMonster(Entity entity) {
+        int duration = 100000;
+        Collection<PotionEffect> effects = new ArrayList<>();
+        effects.add(new PotionEffect(PotionEffectType.SPEED, duration, 2, false, false));
+        effects.add(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration,1, false, false));
 
+        Monster monster = (Monster) entity;
+        monster.addPotionEffects(effects);
     }
-
 }
