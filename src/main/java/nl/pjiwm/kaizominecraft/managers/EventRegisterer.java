de26f1ee921dad2a4e8d247af6c7e9497824b647
PMunne <Pim.Munne@gmail.com>
2021-06-09 18:13:11 +0200

    added event listener for bows and projectiles for monsters

--- src/main/java/nl/pjiwm/kaizominecraft/managers/EventRegisterer.java
+++ src/main/java/nl/pjiwm/kaizominecraft/managers/EventRegisterer.java
@@ -2,6 +2,9 @@ package nl.pjiwm.kaizominecraft.managers;
 
 
 
+import nl.pjiwm.kaizominecraft.listeners.ArrowBuff;
+import nl.pjiwm.kaizominecraft.listeners.ProjectileBuff;
+import nl.pjiwm.kaizominecraft.listeners.SpawnBuffedMob;
 import nl.pjiwm.kaizominecraft.listeners.SpawnCustomMob;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,5 +13,8 @@ public class EventRegisterer {
 
     public EventRegisterer(PluginManager pm, JavaPlugin plugin) {
         pm.registerEvents(new SpawnCustomMob(plugin), plugin);
+        pm.registerEvents(new SpawnBuffedMob(), plugin);
+        pm.registerEvents(new ProjectileBuff(), plugin);
+        pm.registerEvents(new ArrowBuff(), plugin);
     }
 }
