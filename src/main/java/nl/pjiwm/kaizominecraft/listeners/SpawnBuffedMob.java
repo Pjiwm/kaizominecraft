d2b173207075ec588cafa8a8e9e44e918a1fc678
PMunne <Pim.Munne@gmail.com>
2021-06-09 17:21:48 +0200

    gave zombiefied piglin sword buff

--- src/main/java/nl/pjiwm/kaizominecraft/listeners/SpawnBuffedMob.java
+++ src/main/java/nl/pjiwm/kaizominecraft/listeners/SpawnBuffedMob.java
@@ -1,12 +1,16 @@
 package nl.pjiwm.kaizominecraft.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Monster;
+import org.bukkit.entity.PigZombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -18,15 +22,28 @@ public class SpawnBuffedMob implements Listener {
     @EventHandler
     public void spawnEntity(EntitySpawnEvent e) {
         Entity entity = e.getEntity();
-        if(entity instanceof Monster) {
+        if (entity instanceof Monster) {
             buffMonster(entity);
         }
 
-        switch(entity.getType()) {
+        switch (entity.getType()) {
             case CREEPER:
-            Creeper creeper = (Creeper) e.getEntity();
-            buffCreeper(creeper);
+                Creeper creeper = (Creeper) e.getEntity();
+                buffCreeper(creeper);
+            case ZOMBIFIED_PIGLIN:
+                PigZombie pigZombie = (PigZombie) e.getEntity();
+
         }
+    }
+
+    private void buffMonster(Entity entity) {
+        int duration = 100000;
+        Collection<PotionEffect> effects = new ArrayList<>();
+        effects.add(new PotionEffect(PotionEffectType.SPEED, duration, 2, false, false));
+        effects.add(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration, 1, false, false));
+
+        Monster monster = (Monster) entity;
+        monster.addPotionEffects(effects);
     }
 
     private void buffCreeper(Creeper creeper) {
@@ -35,13 +52,10 @@ public class SpawnBuffedMob implements Listener {
         creeper.setExplosionRadius(20);
     }
 
-    private void buffMonster(Entity entity) {
-        int duration = 100000;
-        Collection<PotionEffect> effects = new ArrayList<>();
-        effects.add(new PotionEffect(PotionEffectType.SPEED, duration, 2, false, false));
-        effects.add(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration,1, false, false));
-
-        Monster monster = (Monster) entity;
-        monster.addPotionEffects(effects);
+    private void buffPigZombie(PigZombie pigZombie) {
+        ItemStack stack = new ItemStack(Material.GOLDEN_SWORD, 1);
+        stack.addEnchantment(Enchantment.FIRE_ASPECT, 1);
+        stack.addEnchantment(Enchantment.VANISHING_CURSE, 1);
+        pigZombie.getEquipment().setItemInMainHand(stack);
     }
 }
