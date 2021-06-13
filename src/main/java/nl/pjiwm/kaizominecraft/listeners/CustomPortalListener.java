acc0e07e63937cb7c643ff1cb27941b7e2620588
PMunne <Pim.Munne@gmail.com>
2021-06-13 23:53:15 +0200

    added portal linking from custom worlds

--- src/main/java/nl/pjiwm/kaizominecraft/listeners/CustomPortalListener.java
+++ src/main/java/nl/pjiwm/kaizominecraft/listeners/CustomPortalListener.java
@@ -2,8 +2,11 @@ package nl.pjiwm.kaizominecraft.listeners;
 
 import nl.pjiwm.kaizominecraft.managers.CustomWorldManager;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,47 +19,57 @@ public class CustomPortalListener implements Listener {
     public CustomPortalListener(JavaPlugin plugin) {
         this.server = plugin.getServer();
     }
+//  TODO check what works on this function
 
-//    @EventHandler
-//    public void onPortal(PlayerPortalEvent event) {
-//        Player player = event.getPlayer();
-//
-//        if (event.getCause() == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) {
+// seems like travelAgents is deprecated and the whole agent part isn't necessary anymore
+// code is from spigot forum: https://www.spigotmc.org/threads/create-world-with-nether-and-end.399952/
+// originally from Multiverse might be able to find their current mehod for this event if it doesn't work.
+    /**
+     *
+     * @param event - the event of a player entering a portal
+     */
+    @EventHandler
+    public void onPortal(PlayerPortalEvent event) {
+        Player player = event.getPlayer();
+
+        if (event.getCause() == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) {
 //            event.useTravelAgent(true);
 //            event.getPortalTravelAgent().setCanCreatePortal(true);
-//            Location location;
-//            if (player.getWorld() == getWorld()) {
-//                location = new Location(getNether(), event.getFrom().getBlockX() / 8, event.getFrom().getBlockY(), event.getFrom().getBlockZ() / 8);
-//            } else {
-//                location = new Location(getWorld(), event.getFrom().getBlockX() * 8, event.getFrom().getBlockY(), event.getFrom().getBlockZ() * 8);
-//            }
+            event.setCanCreatePortal(true);
+            Location location;
+            if (player.getWorld() == getWorld()) {
+                location = new Location(getNether(), event.getFrom().getBlockX() / 8, event.getFrom().getBlockY(), event.getFrom().getBlockZ() / 8);
+            } else {
+                location = new Location(getWorld(), event.getFrom().getBlockX() * 8, event.getFrom().getBlockY(), event.getFrom().getBlockZ() * 8);
+            }
 //            event.setTo(event.getPortalTravelAgent().findOrCreate(location));
-//        }
-//
-//        if (event.getCause() == PlayerTeleportEvent.TeleportCause.END_PORTAL) {
-//            if (player.getWorld() == getWorld()) {
-//                Location loc = new Location(getEnd(), 100, 50, 0); // This is the vanilla location for obsidian platform.
-//                event.setTo(loc);
-//                Block block = loc.getBlock();
-//                for (int x = block.getX() - 2; x <= block.getX() + 2; x++) {
-//                    for (int z = block.getZ() - 2; z <= block.getZ() + 2; z++) {
-//                        Block platformBlock = loc.getWorld().getBlockAt(x, block.getY() - 1, z);
-//                        if (platformBlock.getType() != Material.OBSIDIAN) {
-//                            platformBlock.setType(Material.OBSIDIAN);
-//                        }
-//                        for (int yMod = 1; yMod <= 3; yMod++) {
-//                            Block b = platformBlock.getRelative(BlockFace.UP, yMod);
-//                            if (b.getType() != Material.AIR) {
-//                                b.setType(Material.AIR);
-//                            }
-//                        }
-//                    }
-//                }
-//            } else if (player.getWorld() == getEnd()) {
-//                event.setTo(getWorld().getSpawnLocation());
-//            }
-//        }
-//    }
+            event.setTo(location);
+        }
+
+        if (event.getCause() == PlayerTeleportEvent.TeleportCause.END_PORTAL) {
+            if (player.getWorld() == getWorld()) {
+                Location loc = new Location(getEnd(), 100, 50, 0); // This is the vanilla location for obsidian platform.
+                event.setTo(loc);
+                Block block = loc.getBlock();
+                for (int x = block.getX() - 2; x <= block.getX() + 2; x++) {
+                    for (int z = block.getZ() - 2; z <= block.getZ() + 2; z++) {
+                        Block platformBlock = loc.getWorld().getBlockAt(x, block.getY() - 1, z);
+                        if (platformBlock.getType() != Material.OBSIDIAN) {
+                            platformBlock.setType(Material.OBSIDIAN);
+                        }
+                        for (int yMod = 1; yMod <= 3; yMod++) {
+                            Block b = platformBlock.getRelative(BlockFace.UP, yMod);
+                            if (b.getType() != Material.AIR) {
+                                b.setType(Material.AIR);
+                            }
+                        }
+                    }
+                }
+            } else if (player.getWorld() == getEnd()) {
+                event.setTo(getWorld().getSpawnLocation());
+            }
+        }
+    }
 
     private World getWorld() {
         World world;
