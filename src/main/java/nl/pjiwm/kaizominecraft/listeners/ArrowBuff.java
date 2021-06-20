b9d74c1882c1b5ed35cbdf32a2d47573cad92376
PMunne <Pim.Munne@gmail.com>
2021-06-20 21:33:58 +0200

    added feature that links shooting mob to certain tipped arrow

--- src/main/java/nl/pjiwm/kaizominecraft/listeners/ArrowBuff.java
+++ src/main/java/nl/pjiwm/kaizominecraft/listeners/ArrowBuff.java
@@ -20,14 +20,23 @@ import java.util.Random;
 
 public class ArrowBuff implements Listener {
     private NamespacedKey shooterKey;
-    private Map<PotionEffectType, PotionEffect> potionEffects;
-    private JavaPlugin plugin;
+    private Map<String, PotionEffect> potionEffects;
+    private final String[] effectTypes;
 
     public ArrowBuff(JavaPlugin plugin) {
         this.shooterKey = new NamespacedKey(plugin, "shooter");
         this.potionEffects = setPotionEffects();
-        this.plugin = plugin;
+        this.effectTypes = new String[]{
+                PotionEffectType.CONFUSION.toString(),
+                PotionEffectType.BLINDNESS.toString(),
+                PotionEffectType.HUNGER.toString(),
+                PotionEffectType.LEVITATION.toString(),
+                PotionEffectType.WEAKNESS.toString(),
+                PotionEffectType.SLOW.toString(),
+                PotionEffectType.POISON.toString()
+        };
     }
+
     /**
      * Replaces the arrows that have been shot by a bow.
      * The arrows will be given poison and nausea effects.
@@ -41,28 +50,44 @@ public class ArrowBuff implements Listener {
         if (e.getEntity() instanceof Monster) {
             Arrow arrow = (Arrow) e.getProjectile();
             PersistentDataContainer container = e.getEntity().getPersistentDataContainer();
-            e.setProjectile(arrow);
+//            get a potion effect key first if they don't have one yet
+            if (!container.has(shooterKey, PersistentDataType.STRING)) {
+                setKey(e.getEntity());
+            }
+//            if the potion effect (as string) in the key matches their arrow will be changed.
+            for(String s : effectTypes) {
+                if(s.equals(container.get(shooterKey, PersistentDataType.STRING))) {
+                    arrow.addCustomEffect(potionEffects.get(s), true);
+                    e.setProjectile(arrow);
+                    return;
+                }
+            }
         }
     }
 
-    private HashMap<PotionEffectType, PotionEffect> setPotionEffects() {
-        HashMap<PotionEffectType, PotionEffect> potionEffects = new HashMap<>();
+    /**
+     * creates a hashmap that stores different potion effects that can be used on tipped arrows.
+     *
+     * @return - a hashmap with potion effect that can be called upon their PotionEffectType
+     */
+    private HashMap<String, PotionEffect> setPotionEffects() {
+        HashMap<String, PotionEffect> potionEffects = new HashMap<>();
         PotionEffectType type;
 
         type = PotionEffectType.CONFUSION;
-        potionEffects.put(type, new PotionEffect(type, 5, 2));
+        potionEffects.put(type.toString(), new PotionEffect(type, 5, 2));
         type = PotionEffectType.BLINDNESS;
-        potionEffects.put(type, new PotionEffect(type, 4, 1));
+        potionEffects.put(type.toString(), new PotionEffect(type, 4, 1));
         type = PotionEffectType.HUNGER;
-        potionEffects.put(type, new PotionEffect(type, 10, 1));
+        potionEffects.put(type.toString(), new PotionEffect(type, 10, 1));
         type = PotionEffectType.LEVITATION;
-        potionEffects.put(type, new PotionEffect(type, 3, 5));
+        potionEffects.put(type.toString(), new PotionEffect(type, 3, 5));
         type = PotionEffectType.WEAKNESS;
-        potionEffects.put(type, new PotionEffect(type, 15, 1));
+        potionEffects.put(type.toString(), new PotionEffect(type, 15, 1));
         type = PotionEffectType.SLOW;
-        potionEffects.put(type, new PotionEffect(type, 5, 5));
+        potionEffects.put(type.toString(), new PotionEffect(type, 5, 5));
         type = PotionEffectType.POISON;
-        potionEffects.put(type, new PotionEffect(type, 3, 1));
+        potionEffects.put(type.toString(), new PotionEffect(type, 3, 1));
         return potionEffects;
     }
 
@@ -70,19 +95,11 @@ public class ArrowBuff implements Listener {
      * assigns a random effect to the shooterKey of a mob.
      * This way you'll be able to check what type of arrow the mob
      * should be shooting.
+     *
      * @param e - the entity a named space key will be set to.
      */
     private void setKey(Entity e) {
-        String[] effects = {
-                PotionEffectType.CONFUSION.toString(),
-                PotionEffectType.BLINDNESS.toString(),
-                PotionEffectType.HUNGER.toString(),
-                PotionEffectType.LEVITATION.toString(),
-                PotionEffectType.WEAKNESS.toString(),
-                PotionEffectType.SLOW.toString(),
-                PotionEffectType.POISON.toString()
-        };
-        int i = new Random().nextInt(effects.length);
-        e.getPersistentDataContainer().set(shooterKey, PersistentDataType.STRING, effects[i]);
+        int i = new Random().nextInt(effectTypes.length);
+        e.getPersistentDataContainer().set(shooterKey, PersistentDataType.STRING, effectTypes[i]);
     }
 }
