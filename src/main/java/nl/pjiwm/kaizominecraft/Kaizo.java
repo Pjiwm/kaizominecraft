1ff2ba9aebcb5d9ca27648198ac0c2293e559edf
pjiwm <Pim.munne@gmail.com>
2021-07-26 18:56:14 +0200

    added version finder

--- src/main/java/nl/pjiwm/kaizominecraft/Kaizo.java
+++ src/main/java/nl/pjiwm/kaizominecraft/Kaizo.java
@@ -3,8 +3,12 @@ package nl.pjiwm.kaizominecraft;
 import nl.pjiwm.kaizominecraft.managers.CustomMobWorldSpawner;
 import nl.pjiwm.kaizominecraft.managers.CustomWorldManager;
 import nl.pjiwm.kaizominecraft.managers.EventRegisterer;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class Kaizo extends JavaPlugin {
 
@@ -14,11 +18,19 @@ public final class Kaizo extends JavaPlugin {
         CustomWorldManager.geneRateWorlds();
         PluginManager pm = getServer().getPluginManager();
         EventRegisterer.registerEvents(pm, this);
-            CustomMobWorldSpawner.replaceMobs(getServer().getWorld(CustomWorldManager.OVERWORLD_NAME));
+        CustomMobWorldSpawner.replaceMobs(getServer().getWorld(CustomWorldManager.OVERWORLD_NAME));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private String getMinecraftVersion() {
+        Matcher matcher = Pattern.compile("(\\(MC: )([\\d\\.]+)(\\))").matcher(Bukkit.getVersion());
+        if (matcher.find()) {
+            return matcher.group(2);
+        }
+        return null;
     }
 }
---
+++ src/main/java/nl/pjiwm/kaizominecraft/managers/CustomMobManagerFactory.java
@@ -0,0 +1,2 @@
+package nl.pjiwm.kaizominecraft.managers;public interface CustomMobManagerFactory {
+}
